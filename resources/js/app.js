88123c251739338539cd8e87264ff0bd0697f2eb
Abraham_S <abraham7dsm@gmail.com>
2024-12-16 15:43:29 -0400

    loquesea

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,9 @@
 import './bootstrap';
 import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs'
 import { allGames, games1, games2, games3 } from './images';
+import loteriaImg from '../../img/loteria.png'
+import pagosImg from '../../img/pagos.png'
+
 
 $('#play-more').hover(()=>{
   $('#arrow-l').addClass('animate-Hbounce')
@@ -19,16 +22,18 @@ const createSwiper = (name) =>{
   const posiblesGames = {
     'outstanding' : () => games1,
     'new-games' : () => games2,
-    'clasic' : () => games3
+    'clasic' : () => games3,
+    'all-games' : () => allGames
   }
+
   const games = posiblesGames[name]() || allGames
-  
   const fragment = new DocumentFragment()
+  
   for(const game of games){
     const li = document.createElement('li')
     li.classList.add('swiper-slide')
     const div = document.createElement('div')
-    div.classList = 'w-72 h-72 object-contain rounded-xl hover:w-80 hover:h-80 transition-all cursor-pointer'
+    div.classList = 'size-72 object-contain rounded-xl hover:size-80 transition-all cursor-pointer'
     div.style.backgroundImage = `url(${game})`
     div.style.backgroundSize = 'cover' 
     div.style.backgroundPosition = 'center'
@@ -37,8 +42,10 @@ const createSwiper = (name) =>{
   }
 
   const swiperCont = `.${name}-swiper`
+
   const prev = `.${name}-prev`
   const next = `.${name}-next`
+  console.log($(swiperCont))
   $(swiperCont).append(fragment)
   const swiper = `.${name}`
   new Swiper( swiper , {
@@ -56,3 +63,7 @@ const createSwiper = (name) =>{
 createSwiper('outstanding')
 createSwiper('new-games')
 createSwiper('clasic')
+createSwiper('all-games')
+
+$('#loteria').attr('src',loteriaImg)
+$('#pagos').attr('src',pagosImg)
