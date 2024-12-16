6922aa36f93d792f49b1f93ea998c97aa3fa2e0b
Abraham_S <abraham7dsm@gmail.com>
2024-12-16 08:24:30 -0400

    agrega colores personalizados al tema de tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,10 @@ export default {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
+            colors:{
+                'vibrant-pink': '#ff308a', 
+                'orange': '#ff3030'
+            }
         },
     },
     plugins: [],
