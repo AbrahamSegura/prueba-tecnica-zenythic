bb412cb84f561039096d651c7fe7660b79d52b25
Abraham_S <abraham7dsm@gmail.com>
2024-12-16 13:31:29 -0400

    crea una animacion

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,21 @@ export default {
             colors:{
                 'vibrant-pink': '#ff308a', 
                 'orange': '#ff3030'
+            },  
+            animation:{
+                Hbounce: 'Hbounce .4s infinite',
+            },
+            keyframes:{
+                Hbounce: {
+                    '0% , 100%' :{
+                        transform: 'translateX(-5px)',
+                        animationTimingFunction: 'cubic-bezier(0.8, 0, 1, 1)'
+                      },
+                      '50%': {
+                        transform: 'translateX(5px)',
+                        animationTimingFunction: 'cubic-bezier(0, 0, 0.2, 1)'
+                      }
+                }
             }
         },
     },
