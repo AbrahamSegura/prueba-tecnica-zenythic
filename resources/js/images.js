f4a93cc2afb94e21578cb214d1c8377a9d0d1a64
Abraham_S <abraham7dsm@gmail.com>
2024-12-16 13:31:51 -0400

    crea archivo para imagenes

---
+++ resources/js/images.js
@@ -0,0 +1,17 @@
+import img_1 from '../../img/juegos/juego_1.jpg'
+import img_2 from '../../img/juegos/juego_2.jpg'
+import img_3 from '../../img/juegos/juego_3.jpg'
+import img_4 from '../../img/juegos/juego_4.jpg'
+import img_5 from '../../img/juegos/juego_5.jpg'
+import img_6 from '../../img/juegos/juego_6.jpg'
+import img_7 from '../../img/juegos/juego_7.jpg'
+import img_8 from '../../img/juegos/juego_8.jpg'
+import img_9 from '../../img/juegos/juego_9.jpg'
+import img_10 from '../../img/juegos/juego_10.jpg'
+import img_11 from '../../img/juegos/juego_11.jpg'
+import img_12 from '../../img/juegos/juego_12.jpg'
+
+export const allGames = [img_1,img_2,img_3,img_4,img_5,img_6,img_7,img_8,img_9,img_10,img_11,img_12]
+export const games1 = [img_1,img_2,img_3,img_4,img_5]
+export const games2 = [img_5,img_6,img_7,img_8,img_9]
+export const games3 = [img_9,img_10,img_11,img_12,img_1]
