3ec4c93859104b962fa80554285394c3ca1e9b8d
Abraham_S <abraham7dsm@gmail.com>
2024-12-21 22:14:04 -0400

    agrega ruta para deportes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SportsController;
+
+
+use function Pest\Laravel\post;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/front/deportes',SportsController::class);
