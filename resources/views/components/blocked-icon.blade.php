e08c5a0c227d2282e0fce205cad8f17fde2d2a2f
Abraham_S <abraham7dsm@gmail.com>
2024-12-23 02:53:22 -0400

    agrega iconos

---
+++ resources/views/components/blocked-icon.blade.php
@@ -0,0 +1,14 @@
+@props(['width'=>"16px", 'height'=>"16px"])
+
+<svg 
+    width={{$width}} height={{$height}} 
+    viewBox="0 0 16.00 16.00" 
+    xmlns="http://www.w3.org/2000/svg" version="1.1" 
+    fill="none" stroke="#ffffff" stroke-linecap="round" stroke-linejoin="round" stroke-width="0.8">
+    <g stroke-width="0"></g>
+    <g stroke-linecap="round" stroke-linejoin="round" stroke="#CCCCCC" stroke-width="0.192"></g>
+    <g>
+        <rect height="7.5" width="10.5" y="6.75" x="2.75"></rect>
+        <path d="m4.75 6.25s-1-4.5 3.25-4.5 3.25 4.5 3.25 4.5"></path>
+    </g>
+</svg>
