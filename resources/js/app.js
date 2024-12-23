0a81c1905031e93d9c70bafca5b421b70fef3e3a
Abraham_S <abraham7dsm@gmail.com>
2024-12-23 02:54:55 -0400

    crea nuevos swipers

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,44 +2,46 @@ import './bootstrap';
 import $ from 'jquery';
 import Swiper from 'swiper';
 import 'swiper/css'
+import 'swiper/css/autoplay';
+import 'swiper/css/free-mode';
 import 'swiper/css/navigation';
-import { Navigation } from 'swiper/modules';
+import 'swiper/css/scrollbar';
+import { Navigation, Autoplay, Scrollbar, FreeMode } from 'swiper/modules';
 import { allGames, games1, games2, games3 } from './images';
 import loteriaImg from '../../img/loteria.png';
 import pagosImg from '../../img/pagos.png';
 
 
-$('#play-more').hover(()=>{
+$('#play-more').hover(() => {
   $('#arrow-l').addClass('animate-Hbounce')
-}).mouseleave(()=>{
+}).mouseleave(() => {
   $('#arrow-l').removeClass('animate-Hbounce')
 })
 
-new Swiper(".mySwiper", {
-  watchSlidesProgress: true,
-  slidesPerView: 5,
-  spaceBetween: 5,
-  loop: true,
-});
+const heartWraper = document.querySelectorAll('.heart-wraper')
+heartWraper.forEach(hw=> hw.addEventListener('click',()=>{
+  hw.classList.toggle('bg-red-500')
+})
+)
 
-const createSwiper = (name) =>{
+const createSwiper = (name) => {
   const posiblesGames = {
-    'outstanding' : () => games1,
-    'new-games' : () => games2,
-    'clasic' : () => games3,
-    'all-games' : () => allGames
+    'outstanding': () => games1,
+    'new-games': () => games2,
+    'clasic': () => games3,
+    'all-games': () => allGames
   }
 
   const games = posiblesGames[name]() || allGames
   const fragment = new DocumentFragment()
-  
-  for(const game of games){
+
+  for (const game of games) {
     const li = document.createElement('li')
     li.classList.add('swiper-slide')
     const div = document.createElement('div')
-    div.classList = 'size-72 object-contain rounded-xl hover:size-80 transition-all cursor-pointer'
+    div.classList = 'size-72 object-contain rounded-xl hover:scale-110 transition-all cursor-pointer'
     div.style.backgroundImage = `url(${game})`
-    div.style.backgroundSize = 'cover' 
+    div.style.backgroundSize = 'cover'
     div.style.backgroundPosition = 'center'
     li.appendChild(div)
     fragment.appendChild(li)
@@ -51,16 +53,19 @@ const createSwiper = (name) =>{
   const next = `.${name}-next`
   $(swiperCont).append(fragment)
   const swiper = `.${name}`
-  new Swiper( swiper , {
-    modules: [Navigation],
+  new Swiper(swiper, {
+    modules: [Navigation,Autoplay],
     watchSlidesProgress: true,
     slidesPerView: 4,
     spaceBetween: 15,
-    loop:true,
+    loop: true,
+    autoplay: {
+      delay: 1000,
+    },
     navigation: {
       nextEl: next,
       prevEl: prev
-    } 
+    }
   });
 }
 
@@ -69,5 +74,27 @@ createSwiper('new-games')
 createSwiper('clasic')
 createSwiper('all-games')
 
-$('#loteria').attr('src',loteriaImg)
-$('#pagos').attr('src',pagosImg)
+$('#loteria').attr('src', loteriaImg)
+$('#pagos').attr('src', pagosImg)
+
+
+new Swiper('.game-card-swiper', {
+  modules: [Scrollbar, FreeMode],
+  slidesPerView: "auto",
+  spaceBetween: 10,
+  freeMode: {
+    enabled: true
+  },
+  scrollbar:{
+    el: ".game-scrollbar",
+    hide: false 
+  }
+})
+new Swiper('.games-category', {
+  modules: [FreeMode],
+  slidesPerView: "auto",
+  spaceBetween: 10,
+  freeMode: {
+    enabled: true
+  },
+})
